2b76d468276a769b2bdd40c21b9a7229b439990a
stefanytam <stefany.tam.p@gmail.com>
2022-04-07 17:55:25 -0300

    3 commit

---
+++ teste-gft/teste-gft-ex4/teste-gft-ex4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace teste_gft_ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, div;
+            div = 0;
+            do
+            {
+                Console.WriteLine("Digite um valor entre 3 e 999: ");
+                n = int.Parse(Console.ReadLine());
+            } while (n <= 3 || n >= 999);
+            for (int i = 3; i < n; i++)
+            {
+                if (div % i == 0)
+                {
+                    div++;
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
