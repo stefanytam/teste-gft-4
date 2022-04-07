372eff98bf6ded286d631e33238b7e07eec85166
stefanytam <stefany.tam.p@gmail.com>
2022-04-07 17:43:19 -0300

    2 commit

---
+++ teste-gft-ex2/teste-gft-ex2/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿
+namespace teste_gft_ex2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Calculo = new System.Windows.Forms.Button();
+            this.txtAlturaTr = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBaseTr = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLado = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtAlturaRet = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtBaseRet = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtAlturaTra = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtBaseMenoTra = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtBaseMaioTra = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Calculo
+            // 
+            this.btn_Calculo.Location = new System.Drawing.Point(210, 270);
+            this.btn_Calculo.Name = "btn_Calculo";
+            this.btn_Calculo.Size = new System.Drawing.Size(123, 23);
+            this.btn_Calculo.TabIndex = 38;
+            this.btn_Calculo.Text = "Cálculo Geral";
+            this.btn_Calculo.UseVisualStyleBackColor = true;
+            this.btn_Calculo.Click += new System.EventHandler(this.btn_Calculo_Click);
+            // 
+            // txtAlturaTr
+            // 
+            this.txtAlturaTr.Location = new System.Drawing.Point(140, 148);
+            this.txtAlturaTr.Name = "txtAlturaTr";
+            this.txtAlturaTr.Size = new System.Drawing.Size(74, 23);
+            this.txtAlturaTr.TabIndex = 37;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(83, 15);
+            this.label3.TabIndex = 36;
+            this.label3.Text = "Digite a altura:";
+            // 
+            // txtBaseTr
+            // 
+            this.txtBaseTr.Location = new System.Drawing.Point(139, 122);
+            this.txtBaseTr.Name = "txtBaseTr";
+            this.txtBaseTr.Size = new System.Drawing.Size(74, 23);
+            this.txtBaseTr.TabIndex = 35;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(57, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 15);
+            this.label2.TabIndex = 34;
+            this.label2.Text = "Digite a base:";
+            // 
+            // txtLado
+            // 
+            this.txtLado.Location = new System.Drawing.Point(139, 51);
+            this.txtLado.Name = "txtLado";
+            this.txtLado.Size = new System.Drawing.Size(74, 23);
+            this.txtLado.TabIndex = 30;
+            this.txtLado.TextChanged += new System.EventHandler(this.txtLado_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 15);
+            this.label1.TabIndex = 29;
+            this.label1.Text = "Lado:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 98);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 15);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "Triângulo";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(56, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 15);
+            this.label5.TabIndex = 40;
+            this.label5.Text = "Quadrado";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(305, 22);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 15);
+            this.label6.TabIndex = 45;
+            this.label6.Text = "Retângulo";
+            // 
+            // txtAlturaRet
+            // 
+            this.txtAlturaRet.Location = new System.Drawing.Point(388, 72);
+            this.txtAlturaRet.Name = "txtAlturaRet";
+            this.txtAlturaRet.Size = new System.Drawing.Size(74, 23);
+            this.txtAlturaRet.TabIndex = 44;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(305, 75);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 15);
+            this.label7.TabIndex = 43;
+            this.label7.Text = "Digite a altura:";
+            // 
+            // txtBaseRet
+            // 
+            this.txtBaseRet.Location = new System.Drawing.Point(387, 46);
+            this.txtBaseRet.Name = "txtBaseRet";
+            this.txtBaseRet.Size = new System.Drawing.Size(74, 23);
+            this.txtBaseRet.TabIndex = 42;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(305, 46);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(77, 15);
+            this.label8.TabIndex = 41;
+            this.label8.Text = "Digite a base:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(305, 107);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 15);
+            this.label9.TabIndex = 50;
+            this.label9.Text = "Trapézio";
+            // 
+            // txtAlturaTra
+            // 
+            this.txtAlturaTra.Location = new System.Drawing.Point(426, 200);
+            this.txtAlturaTra.Name = "txtAlturaTra";
+            this.txtAlturaTra.Size = new System.Drawing.Size(74, 23);
+            this.txtAlturaTra.TabIndex = 49;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(305, 200);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(83, 15);
+            this.label10.TabIndex = 48;
+            this.label10.Text = "Digite a altura:";
+            // 
+            // txtBaseMenoTra
+            // 
+            this.txtBaseMenoTra.Location = new System.Drawing.Point(426, 128);
+            this.txtBaseMenoTra.Name = "txtBaseMenoTra";
+            this.txtBaseMenoTra.Size = new System.Drawing.Size(74, 23);
+            this.txtBaseMenoTra.TabIndex = 47;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(305, 131);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(115, 15);
+            this.label11.TabIndex = 46;
+            this.label11.Text = "Digite a base menor:";
+            // 
+            // txtBaseMaioTra
+            // 
+            this.txtBaseMaioTra.Location = new System.Drawing.Point(426, 165);
+            this.txtBaseMaioTra.Name = "txtBaseMaioTra";
+            this.txtBaseMaioTra.Size = new System.Drawing.Size(74, 23);
+            this.txtBaseMaioTra.TabIndex = 52;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(305, 168);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(111, 15);
+            this.label12.TabIndex = 51;
+            this.label12.Text = "Digite a base maior:";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(57, 203);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(45, 15);
+            this.lblResult.TabIndex = 53;
+            this.lblResult.Text = "ÁREAS:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 316);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtBaseMaioTra);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtAlturaTra);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtBaseMenoTra);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtAlturaRet);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtBaseRet);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btn_Calculo);
+            this.Controls.Add(this.txtAlturaTr);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtBaseTr);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtLado);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Quadrado";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_Calculo;
+        private System.Windows.Forms.TextBox txtAlturaTr;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBaseTr;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLado;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtAlturaRet;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtBaseRet;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtAlturaTra;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtBaseMenoTra;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtBaseMaioTra;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ teste-gft-ex2/teste-gft-ex2/Form1.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace teste_gft_ex2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtLado_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public abstract class Forma
+        {
+            private double area;
+           
+            public double Area
+            {
+                get
+                { return area; }
+                set
+                { area = value; }
+            }
+            public abstract void CalcularArea();
+        }
+
+        public class Quadrado : Forma
+        {
+            private double lado;
+            public double Lado
+            {
+                get
+                { return lado; }
+                set
+                { lado = value; }
+            }
+            public override void CalcularArea()
+            {
+                this.Area = lado * lado;
+            }
+        }
+        public class Triangulo : Forma
+        {
+            private double baseTr;
+            private double alturaTr;
+
+            public double BaseTr
+            {
+                get
+                { return baseTr; }
+                set
+                { baseTr = value; }
+            }
+            public double AlturaTr
+            {
+                get
+                { return alturaTr; }
+                set
+                { alturaTr = value; }
+            }
+
+            public override void CalcularArea()
+            {
+                this.Area = (baseTr*alturaTr)/2;
+            }
+        }
+
+        public class Retangulo : Forma
+        {
+            private double baseRet;
+            private double alturaRet;
+
+            public double BaseRet
+            {
+                get
+                { return baseRet; }
+                set
+                { baseRet = value; }
+            }
+            public double AlturaRet
+            {
+                get
+                { return alturaRet; }
+                set
+                { alturaRet = value; }
+            }
+
+            public override void CalcularArea()
+            {
+                this.Area = baseRet * alturaRet;
+            }
+        }
+
+        public class Trapezio : Forma
+        {
+            private double baseMenoTra;
+            private double baseMaioTra;
+            private double alturaTra;
+
+            public double BaseMenoTra
+            {
+                get
+                { return baseMenoTra; }
+                set
+                { baseMenoTra = value; }
+            }
+            public double BaseMaioTra
+            {
+                get
+                { return baseMaioTra; }
+                set
+                { baseMaioTra = value; }
+            }
+            public double AlturaTra
+            {
+                get
+                { return alturaTra; }
+                set
+                { alturaTra = value; }
+            }
+
+            public override void CalcularArea()
+            {
+                this.Area = ((baseMenoTra + baseMaioTra)*alturaTra)/2;
+            }
+        }
+        Quadrado q = new Quadrado();
+        Retangulo r = new Retangulo();
+        Triangulo tr = new Triangulo();
+        Trapezio tra = new Trapezio();
+        private void btn_Calculo_Click(object sender, EventArgs e)
+        {
+            q.Lado = Convert.ToDouble(txtLado.Text);
+
+            r.BaseRet= Convert.ToDouble(txtBaseRet.Text);
+            r.AlturaRet = Convert.ToDouble(txtAlturaRet.Text);
+
+            tr.BaseTr = Convert.ToDouble(txtBaseTr.Text);
+            tr.AlturaTr = Convert.ToDouble(txtAlturaTr.Text);
+
+            tra.BaseMenoTra = Convert.ToDouble(txtBaseMenoTra.Text);
+            tra.BaseMaioTra = Convert.ToDouble(txtBaseMaioTra.Text);
+            tra.AlturaTra = Convert.ToDouble(txtAlturaTra.Text);
+
+            q.CalcularArea();
+
+            r.CalcularArea();
+
+            tr.CalcularArea();
+
+            tra.CalcularArea();
+
+            lblResult.Text = "Quadrado: " + q.Area + "\nRetângulo: " + r.Area
+            + "\nTriângulo: " + tr.Area + "\nTrapézio: " + tra.Area;
+        }
+    }
+}
