f0c716d140d3ed947aa0f2c7573c4001d8dc78db
stefanytam <stefany.tam.p@gmail.com>
2022-04-07 15:53:54 -0300

    first commit

---
+++ teste-gft/teste-gft/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace teste_gft
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
